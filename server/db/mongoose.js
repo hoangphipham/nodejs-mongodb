c18b9c4ffb71f9fd2fdbf113bf401975eed43155
MrPhiPhi\PhiPhi <phamnguyenhoangphi@gmail.com>
2018-01-05 21:55:13 +0700

    desploy heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI);
+// mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect('mongodb://phipham:Hoang05phi12@ds157653.mlab.com:57653/node-todo-api')
 
 module.exports = {mongoose};
